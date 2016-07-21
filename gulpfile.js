ae689c2982bca9e9644c51a8f0c247bc09c10d7c
Alexandre Paquette <alexandre.paquette@nadrox.com>
2016-07-20 22:38:12 -0400

    basic gulp setup for dev is done

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,43 @@
-var $ = require('gulp-load-plugins)');
-var config = require('gulp.config.json');
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+var config = require('./gulp.config.json');
+var browserSync = require('browser-sync');
 
-var build = gulp.series(app, styles);
-
+function vendorsJs() {
+    return gulp.src(config.paths.src.vendorJs)
+        .pipe($.concat(config.packageName + '.vendors.min.js'))
+        .pipe(gulp.dest(config.paths.dist.js));
+}
 function app() {
     return gulp.src(config.paths.src.js)
         .pipe($.ngAnnotate())
+        .pipe($.angularEmbedTemplates())
         .pipe($.babel())
         .pipe($.uglify())
         .pipe($.concat(config.packageName + '.min.js'))
-        .dest(config.paths.dist.js);
+        .pipe(gulp.dest(config.paths.dist.js));
 }
 function styles() {
     return gulp.src(config.paths.src.less)
         .pipe($.less())
         .pipe($.cleanCss())
         .pipe($.concat(config.packageName + '.min.css'))
-        .dest(config.paths.dist.css);
+        .pipe(gulp.dest(config.paths.dist.css));
+}
+function watch(cb) {
+    gulp.watch(config.paths.src.less, styles);
+    gulp.watch(config.paths.src.js, app);
+
+    cb();
 }
 
-gulp.task('default', build);
+function serve(cb) {
+    browserSync({
+        files: [config.paths.dist.js + '**.js', config.paths.dist.css + '**.css'],
+        server: {
+            baseDir: '.'
+        }
+    });
+    cb();
+}
+gulp.task('default', gulp.series(vendorsJs, app, styles, watch, serve));
---
+++ src/components/ow-hero-picker.js
@@ -0,0 +1,3 @@
+angular.module('overwatch-hero-picker').conponent('owHeroPicker',{
+    template: 'ow-hero-picker.html'
+});
