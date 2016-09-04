c12edec4cd21387a3924ca9962d9eaa966ae76d4
Alexandre Paquette <alexandre.paquette@nadrox.com>
2016-09-04 03:15:27 -0400

    added Eichenwalde map

--- src/LocalStorageKeys.js
+++ src/LocalStorageKeys.js
@@ -1,4 +1,4 @@
 angular.module('overwatch-hero-picker').constant('LocalStorageKeys', {
     heroesRatings: 'ow-player-hero-rating',
-    helpBoxRememberClose: 'help-box-remember-close'
+    helpBoxRememberClose: 'help-box-remember-close-v2'
 });
